36f4a389d1e40ad74e773d58673f5e551280f0bf
rubycouzcedric <cousin.ruby.cedric@gmail.com>
2018-07-31 10:42:40 +0200

    correction

--- index.php
+++ index.php
@@ -1,20 +1,25 @@
 <?php
 $number = 0; //définition de la variable
- ?>
- <!DOCTYPE html>
- <html lang="fr" dir="ltr">
-   <head>
-     <meta charset="utf-8" />
-     <title>Exercice 1</title>
-   </head>
-   <body>
-     <p>
-<?php
-while ($number <= 10) //début de la boucle : tant que la variable est inférieur à 10
-{
-  echo $number++; // incrémentation de la variable
-}
- ?>
-     </p>
-   </body>
- </html>
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8" />
+  <title>Exercice 1</title>
+</head>
+<body>
+  <p>
+    <?php
+    while ($number <= 10) //début de la boucle : tant que la variable est inférieur à 10
+    {
+      echo $number++; // incrémentation de la variable
+    }
+    /*
+    for($number = 0; number <= 10; $number++)
+    {
+    echo $number . ' ';
+  }
+    ?>
+  </p>
+</body>
+</html>
